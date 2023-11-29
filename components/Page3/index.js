d3191fbea76e2fe40e42a87469f5c07377118262
Franulo <franciszek@balamut.eu>
2023-11-29 17:21:13 +0100

    ad page3 and remove scrollbar

--- components/Page3/index.js
+++ components/Page3/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import {View, Text} from 'react-native';
 import styles from "./styles";
 
-const Page2 = () => {
+const Page3 = () => {
 
    return (
       <View style={styles.container}>
@@ -11,4 +11,4 @@ const Page2 = () => {
    );
 };
    
-export default Page2;
+export default Page3;
--- components/PageList/index.js
+++ components/PageList/index.js
@@ -3,6 +3,7 @@ import {Dimensions, ScrollView} from 'react-native';
 import styles from "./style";
 import Page1 from "../Page1";
 import Page2 from "../Page2";
+import Page3 from "../Page3";
 
 const PageList = () => {
 
@@ -13,7 +14,9 @@ const PageList = () => {
          snapToAlignment={"center"} 
          decelerationRate={0}
          style={styles.container}
+         showsHorizontalScrollIndicator={false}
       >
+         <Page3 />
          <Page1 />
          <Page2 />
       </ScrollView>
