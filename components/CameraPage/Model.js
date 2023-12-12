eb18785323b24b8d234a90ea23e32b2202d421d9
Franulo <franciszek@balamut.eu>
2023-12-12 13:12:57 +0100

    trying to debug model.js

--- components/CameraPage/Model.js
+++ components/CameraPage/Model.js
@@ -5,6 +5,7 @@ import { bundleResourceIO } from '@tensorflow/tfjs-react-native';
 import * as jpeg from 'jpeg-js';
 import { Buffer } from "buffer";
 import { convert } from "base64-to-tensor";
+import * as FileSystem from 'expo-file-system';
 
 
 import styles from './styles';
@@ -19,13 +20,26 @@ const model = async function (inputImage) {
  
    const lovefinder = await tf.loadLayersModel(bundleResourceIO(modelJson, modelWeight));
 
+   //console.warn(inputImage.uri)
+
    const height = inputImage.height;
    const width = inputImage.width;
-   const base64 = inputImage.base64;
-   const bytes = Buffer.from(base64, "base64")
-   const imageTensor = tf.tensor(bytes);
-   console.warn(imageTensor[0]);
-   console.warn(height, width)
+
+   const file = await FileSystem.readAsStringAsync(inputImage.uri, {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+
+   console.log('Read image preview:', file.slice(0, 100));
+   console.log('Read image size:', file.length);
+
+   const imageBuffer = tf.util.encodeString(file, 'base64').buffer;
+   console.log('Buffer size:', imageBuffer.byteLength);
+   console.log('Bytes preview:', Array.from(new Uint8Array(imageBuffer)).slice(0, 20));
+
+   const bytes = new Uint8Array(imageBuffer);
+   console.log('Bytes preview:', bytes.slice(0, 50));
+
+   const imageTensor = tf.tensor(bytes, [height, width, 3]);
  
    return inputImage;
  };
